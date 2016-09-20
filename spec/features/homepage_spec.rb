6b1d54ffff28ddb6c6f4acb2d60794e80adcbd61
Kyle Stengline <kyle.stengline@gmail.com>
2016-09-20 15:26:19 -0400

    adds feature test for homepage

---
+++ spec/features/homepage_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.feature "Vist Homepage" do
+
+  scenario "Users can vist the homepage and click sign in and sign out" do
+
+    vist "/"
+
+    expect(page).to have_link "Sign In"
+    expect(page).to have_link "Sign Up"
+    expect(current_path).to eq(root_path) 
+  end
+end
