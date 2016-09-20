8b32c5007f6a0be767b8d15c473621b94c9b4217
Kyle Stengline <kyle.stengline@gmail.com>
2016-09-20 15:34:14 -0400

    updates index page and homepage spec

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
-RSpec.feature "Vist Homepage" do
+RSpec.feature "Visit Homepage" do
 
-  scenario "Users can vist the homepage and click sign in and sign out" do
+  scenario "Users can vist the homepage and click sign in and sign up" do
 
     visit "/"
 
+    expect(page).to have_content "Keep In Touch Home Page"
     expect(page).to have_link "Sign In"
     expect(page).to have_link "Sign Up"
     expect(current_path).to eq(root_path) 
